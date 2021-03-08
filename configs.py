9aee6a73ae4cfe17013ba6d16f5ac2612b013f2c
Abir Hasan <hackitabirhasan@gmail.com>
2021-03-09 03:48:47 +0600

    ☹️

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL" None)
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
 	ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot!
