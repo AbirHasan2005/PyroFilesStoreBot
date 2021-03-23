f69364fb5daf9959194880c20046f462c74cc9d2
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-03-23 21:37:23 +0530

    Audio also is lub 💓
    Why u cruel removed audio 🙄😥

--- bot.py
+++ bot.py
@@ -158,7 +158,7 @@ async def start(bot, cmd):
 		except Exception as err:
 			await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
 
-@Bot.on_message(filters.video | filters.document & ~filters.edited)
+@Bot.on_message(filters.document | filters.video | filters.audio & ~filters.edited)
 async def main(bot, message):
 	if message.chat.type == "private":
 		editable = await message.reply_text("Please wait ...")
