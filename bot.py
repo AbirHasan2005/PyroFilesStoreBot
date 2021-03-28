7435f687c272678e951bcc12d0daa0d4225f141b
Sonia806 <79003788+Sonia806@users.noreply.github.com>
2021-03-29 01:16:49 +0530

    Personalized it

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def start(bot, cmd):
 				if user.status == "kicked":
 					await bot.send_message(
 						chat_id=cmd.from_user.id,
-						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/MovieBuzzOfficialGroup).",
 						parse_mode="markdown",
 						disable_web_page_preview=True
 					)
