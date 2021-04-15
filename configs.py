b6daac71dd3896a9a91f29da6a6fa60a2d6b3793
Abir Hasan <hackitabirhasan@gmail.com>
2021-04-15 21:01:40 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config(object):
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", None).split())
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
