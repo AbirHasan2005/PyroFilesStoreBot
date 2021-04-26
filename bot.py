bb06657c056191aa6fe2fc009265ad75140af960
AbirHasan2005 <hackitabirhasan@gmail.com>
2021-04-27 03:05:06 +0600

    New Update!

--- bot.py
+++ bot.py
@@ -1,55 +1,18 @@
 # (c) @AbirHasan2005
 
 import os
-import time
-import math
-import json
-import string
-import random
 import traceback
-import asyncio
 import datetime
-import aiofiles
-from random import choice
 from pyrogram import Client, filters
+from pyrogram.errors import UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, \
-    PeerIdInvalid
 from configs import Config
+from handlers.force_sub_handler import handle_force_sub
+from handlers.broadcast_handlers import main_broadcast_handler
 from database import Database
 
-## --- Sub Configs --- ##
-BOT_USERNAME = Config.BOT_USERNAME
-BOT_TOKEN = Config.BOT_TOKEN
-API_ID = Config.API_ID
-API_HASH = Config.API_HASH
-DB_CHANNEL = Config.DB_CHANNEL
-ABOUT_BOT_TEXT = Config.ABOUT_BOT_TEXT
-ABOUT_DEV_TEXT = Config.ABOUT_DEV_TEXT
-HOME_TEXT = Config.HOME_TEXT
-BOT_OWNER = Config.BOT_OWNER
-FORWARD_AS_COPY = Config.FORWARD_AS_COPY
-db = Database(Config.DATABASE_URL, BOT_USERNAME)
-broadcast_ids = {}
-Bot = Client(BOT_USERNAME, bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
-
-
-async def send_msg(user_id, message):
-    try:
-        await message.forward(chat_id=user_id)
-        return 200, None
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
-    except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
-    except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
-    except Exception as e:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
+db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
+Bot = Client(Config.BOT_USERNAME, bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH)
 
 
 async def foo(bot, cmd):
@@ -58,7 +21,7 @@ async def foo(bot, cmd):
         await db.add_user(chat_id)
         await bot.send_message(
             Config.LOG_CHANNEL,
-            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!"
+            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
         )
 
     ban_status = await db.get_ban_status(chat_id)
@@ -85,45 +48,17 @@ async def start(bot, cmd):
         return
     usr_cmd = cmd.text.split("_")[-1]
     if usr_cmd == "/start":
-        if not Config.UPDATES_CHANNEL is None:
-            invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
-            try:
-                user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.from_user.id)
-                if user.status == "kicked":
-                    await bot.send_message(
-                        chat_id=cmd.from_user.id,
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-                        parse_mode="markdown",
-                        disable_web_page_preview=True
-                    )
-                    return
-            except UserNotParticipant:
-                await bot.send_message(
-                    chat_id=cmd.from_user.id,
-                    text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
-                            ],
-                            [
-                                InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
-                            ]
-                        ]
-                    ),
-                    parse_mode="markdown"
-                )
-                return
-            except Exception:
-                await bot.send_message(
-                    chat_id=cmd.from_user.id,
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-                    parse_mode="markdown",
-                    disable_web_page_preview=True
-                )
-                return
+        chat_id = cmd.from_user.id
+        if not await db.is_user_exist(chat_id):
+            await db.add_user(chat_id)
+            await bot.send_message(
+                Config.LOG_CHANNEL,
+                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+            )
+        if Config.UPDATES_CHANNEL is not None:
+            await handle_force_sub(bot, cmd)
         await cmd.reply_text(
-            HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
+            Config.HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -140,102 +75,36 @@ async def start(bot, cmd):
             )
         )
     else:
-        if not Config.UPDATES_CHANNEL is None:
-            invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
-            try:
-                user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.from_user.id)
-                if user.status == "kicked":
-                    await bot.send_message(
-                        chat_id=cmd.from_user.id,
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-                        parse_mode="markdown",
-                        disable_web_page_preview=True
-                    )
-                    return
-            except UserNotParticipant:
-                file_id = int(usr_cmd)
-                await bot.send_message(
-                    chat_id=cmd.from_user.id,
-                    text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
-                            ],
-                            [
-                                InlineKeyboardButton("🔄 Refresh / Try Again",
-                                                     url=f"https://telegram.dog/{BOT_USERNAME}?start=AbirHasan2005_{file_id}")
-                            ]
-                        ]
-                    ),
-                    parse_mode="markdown"
-                )
-                return
-            except Exception:
-                await bot.send_message(
-                    chat_id=cmd.from_user.id,
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-                    parse_mode="markdown",
-                    disable_web_page_preview=True
-                )
-                return
+        if Config.UPDATES_CHANNEL is not None:
+            await handle_force_sub(bot, cmd)
         try:
             file_id = int(usr_cmd)
             send_stored_file = None
-            if FORWARD_AS_COPY is True:
-                send_stored_file = await bot.copy_message(chat_id=cmd.from_user.id, from_chat_id=DB_CHANNEL,
+            if Config.FORWARD_AS_COPY is True:
+                send_stored_file = await bot.copy_message(chat_id=cmd.from_user.id, from_chat_id=Config.DB_CHANNEL,
                                                           message_id=file_id)
-            elif FORWARD_AS_COPY is False:
-                send_stored_file = await bot.forward_messages(chat_id=cmd.from_user.id, from_chat_id=DB_CHANNEL,
+            elif Config.FORWARD_AS_COPY is False:
+                send_stored_file = await bot.forward_messages(chat_id=cmd.from_user.id, from_chat_id=Config.DB_CHANNEL,
                                                               message_ids=file_id)
             await send_stored_file.reply_text(
-                f"**Here is Sharable Link of this file:** https://telegram.dog/{BOT_USERNAME}?start=AbirHasan2005_{file_id}\n\n__To Retrive the Stored File, just open the link!__",
+                f"**Here is Sharable Link of this file:** https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{file_id}\n\n__To Retrive the Stored File, just open the link!__",
                 disable_web_page_preview=True, quote=True)
         except Exception as err:
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
 
 
-@Bot.on_message(filters.document | filters.video | filters.audio & ~filters.edited)
+@Bot.on_message((filters.document | filters.video | filters.audio) & ~filters.edited)
 async def main(bot, message):
     if message.chat.type == "private":
-        if not Config.UPDATES_CHANNEL is None:
-            invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
-            try:
-                user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
-                if user.status == "kicked":
-                    await bot.send_message(
-                        chat_id=message.from_user.id,
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-                        parse_mode="markdown",
-                        disable_web_page_preview=True
-                    )
-                    return
-            except UserNotParticipant:
-                await bot.send_message(
-                    chat_id=message.from_user.id,
-                    text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
-                            ],
-                            [
-                                InlineKeyboardButton("🔄 Refresh / Try Again", callback_data="refreshmeh")
-                            ]
-                        ]
-                    ),
-                    parse_mode="markdown"
-                )
-                return
-            except Exception:
-                await bot.send_message(
-                    chat_id=message.from_user.id,
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-                    parse_mode="markdown",
-                    disable_web_page_preview=True
-                )
-                return
-
+        chat_id = message.from_user.id
+        if not await db.is_user_exist(chat_id):
+            await db.add_user(chat_id)
+            await bot.send_message(
+                Config.LOG_CHANNEL,
+                f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{Config.BOT_USERNAME} !!"
+            )
+        if Config.UPDATES_CHANNEL is not None:
+            await handle_force_sub(bot, message)
         if message.from_user.id in Config.BANNED_USERS:
             await message.reply_text("Sorry, You are banned!\n\nContact [Support Group](https://t.me/linux_repo)",
                                      disable_web_page_preview=True)
@@ -244,12 +113,12 @@ async def main(bot, message):
             return
         editable = await message.reply_text("Please wait ...")
         try:
-            forwarded_msg = await message.forward(DB_CHANNEL)
+            forwarded_msg = await message.forward(Config.DB_CHANNEL)
             file_er_id = forwarded_msg.message_id
             await forwarded_msg.reply_text(
                 f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
                 parse_mode="Markdown", disable_web_page_preview=True)
-            share_link = f"https://telegram.dog/{BOT_USERNAME}?start=AbirHasan2005_{file_er_id}"
+            share_link = f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{file_er_id}"
             await editable.edit(
                 f"**Your File Stored in my Database!**\n\nHere is the Permanent Link of your file: {share_link} \n\nJust Click the link to get your file!",
                 parse_mode="Markdown",
@@ -283,9 +152,9 @@ async def main(bot, message):
         if message.photo:
             return
         try:
-            forwarded_msg = await message.forward(DB_CHANNEL)
+            forwarded_msg = await message.forward(Config.DB_CHANNEL)
             file_er_id = forwarded_msg.message_id
-            share_link = f"https://telegram.dog/{BOT_USERNAME}?start=AbirHasan2005_{file_er_id}"
+            share_link = f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{file_er_id}"
             CH_edit = await bot.edit_message_reply_markup(message.chat.id, message.message_id,
                                                           reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
                                                               "Get Sharable Stored Link", url=share_link)]]))
@@ -300,79 +169,18 @@ async def main(bot, message):
             print(f"Error: {err}")
 
 
-@Bot.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply)
-async def broadcast_(c, m):
-    all_users = await db.get_all_users()
-    broadcast_msg = m.reply_to_message
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    out = await m.reply_text(
-        text=f"Broadcast Started! You will be notified with log file when all the users are notified."
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    failed = 0
-    success = 0
-    broadcast_ids[broadcast_id] = dict(
-        total=total_users,
-        current=done,
-        failed=failed,
-        success=success
-    )
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(
-                user_id=int(user['id']),
-                message=broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            if sts == 400:
-                await db.delete_user(user['id'])
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current=done,
-                        failed=failed,
-                        success=success
-                    )
-                )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await asyncio.sleep(3)
-    await out.delete()
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    else:
-        await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    os.remove('broadcast.txt')
+@Bot.on_message(filters.private & filters.command("broadcast") & filters.user(Config.BOT_OWNER) & filters.reply)
+async def broadcast_handler_open(c, m):
+    await main_broadcast_handler(m, db)
 
 
-@Bot.on_message(filters.private & filters.command("status") & filters.user(BOT_OWNER))
+@Bot.on_message(filters.private & filters.command("status") & filters.user(Config.BOT_OWNER))
 async def sts(c, m):
     total_users = await db.total_users_count()
     await m.reply_text(text=f"**Total Users in DB:** `{total_users}`", parse_mode="Markdown", quote=True)
 
 
-@Bot.on_message(filters.private & filters.command("ban_user") & filters.user(BOT_OWNER))
+@Bot.on_message(filters.private & filters.command("ban_user") & filters.user(Config.BOT_OWNER))
 async def ban(c, m):
     if len(m.command) == 1:
         await m.reply_text(
@@ -408,7 +216,7 @@ async def ban(c, m):
         )
 
 
-@Bot.on_message(filters.private & filters.command("unban_user") & filters.user(BOT_OWNER))
+@Bot.on_message(filters.private & filters.command("unban_user") & filters.user(Config.BOT_OWNER))
 async def unban(c, m):
     if len(m.command) == 1:
         await m.reply_text(
@@ -442,7 +250,7 @@ async def unban(c, m):
         )
 
 
-@Bot.on_message(filters.private & filters.command("banned_users") & filters.user(BOT_OWNER))
+@Bot.on_message(filters.private & filters.command("banned_users") & filters.user(Config.BOT_OWNER))
 async def _banned_usrs(c, m):
     all_banned_users = await db.get_all_banned_users()
     banned_usr_count = 0
@@ -469,7 +277,7 @@ async def button(bot, cmd: CallbackQuery):
     cb_data = cmd.data
     if "aboutbot" in cb_data:
         await cmd.message.edit(
-            ABOUT_BOT_TEXT,
+            Config.ABOUT_BOT_TEXT,
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -487,7 +295,7 @@ async def button(bot, cmd: CallbackQuery):
         )
     elif "aboutdevs" in cb_data:
         await cmd.message.edit(
-            ABOUT_DEV_TEXT,
+            Config.ABOUT_DEV_TEXT,
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -505,7 +313,7 @@ async def button(bot, cmd: CallbackQuery):
         )
     elif "gotohome" in cb_data:
         await cmd.message.edit(
-            HOME_TEXT.format(cmd.message.chat.first_name, cmd.message.chat.id),
+            Config.HOME_TEXT.format(cmd.message.chat.first_name, cmd.message.chat.id),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -557,7 +365,7 @@ async def button(bot, cmd: CallbackQuery):
                 )
                 return
         await cmd.message.edit(
-            text=HOME_TEXT.format(cmd.message.chat.first_name, cmd.message.chat.id),
+            text=Config.HOME_TEXT.format(cmd.message.chat.first_name, cmd.message.chat.id),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
---
+++ handlers/force_sub_handler.py
@@ -0,0 +1,44 @@
+# (c) @AbirHasan2005
+
+from configs import Config
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def handle_force_sub(bot, cmd):
+    invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
+    try:
+        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=cmd.from_user.id,
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return
+    except UserNotParticipant:
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                    ],
+                    [
+                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
+                    ]
+                ]
+            ),
+            parse_mode="markdown"
+        )
+        return
+    except Exception:
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            parse_mode="markdown",
+            disable_web_page_preview=True
+        )
+        return
