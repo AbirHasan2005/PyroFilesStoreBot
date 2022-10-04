70793aff851722d5d6464541a38eed6bdf0d6339
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-04 19:56:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,7 +107,7 @@ async def start(bot: Client, cmd: Message):
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
 
 
-@Bot.on_message((filters.document | filters.video | filters.audio | filters.text) & ~filters.chat(Config.DB_CHANNEL))
+@Bot.on_message((filters.document | filters.video | filters.audio | filters.incoming) & ~filters.chat(Config.DB_CHANNEL))
 async def main(bot: Client, message: Message):
 
     if message.chat.type == enums.ChatType.PRIVATE:
