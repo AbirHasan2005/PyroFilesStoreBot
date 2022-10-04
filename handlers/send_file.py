b37e9d880e4673346b3fd148cdbc4a70730c66a3
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-04 21:34:11 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -35,5 +35,5 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(message=sent_message, file_id=file_id)
-    await asyncio.sleep(2)
+    #await reply_forward(message=sent_message, file_id=file_id)
+    await asyncio.sleep(1)
