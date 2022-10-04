7f54af2bfe6bc2185e18ba306c3337e746d9d2f2
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-04 22:38:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,19 +4,19 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))
+	API_ID = int(os.environ.get("API_ID","0"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL","-100"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS").split())
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS","123648594").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS","-100747838838").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is Permanent Files and text Store Bot!
