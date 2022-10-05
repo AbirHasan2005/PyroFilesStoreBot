50f2808ea03bdfe7d64f2a51b35262388bc22d3d
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-05 17:35:42 +0530

    Update remove_word.py

--- handlers/remove_word.py
+++ handlers/remove_word.py
@@ -1,6 +1,9 @@
 from configs import Config
 async def rw(capt):
   for i in Config.REMOVE_WORD:
-    capt1 = capt.replace(f"{i}","")
-    capt = capt1
+    try:
+      capt1 = capt.replace(f"{i}","")
+      capt = capt1
+    except:
+      pass
   return capt
