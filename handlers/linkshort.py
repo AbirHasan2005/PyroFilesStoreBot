cc29d115756b37c1d88dab34e567db31feb79ff4
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-05 11:55:45 +0530

    Update linkshort.py

--- handlers/linkshort.py
+++ handlers/linkshort.py
@@ -2,8 +2,8 @@ from requests import get as sget
 from configs import Config
 async def Short(url):
     data = {}
-    data['api'] = shortner_api
+    data['api'] = SHORTNER_API
     data['url'] = url
     data['format'] = 'text'
-    link = sget(shortner_api_link, params = data).text
+    link = sget(SHORTNER_API_LINK, params = data).text
     return link
