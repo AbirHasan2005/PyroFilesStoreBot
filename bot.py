2d9747684e8a5dd2158044ee3424776df61d544e
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-05 19:47:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,7 +96,7 @@ async def start(bot: Client, cmd: Message):
             print(GetMessage)
             message_ids = []
             if GetMessage.text:
-                message_ids = GetMessage.text.split(" ")
+                message_ids.append(GetMessage.id) #= GetMessage.text.split(" ")
                 _response_msg = await cmd.reply_text(
                     text=f"**Total Files:** `{len(message_ids)}`",
                     quote=True,
